87fda145ddba7540ccf343fc4397c22e11b2384b
Sumedha Mehta <sumedham@fb.com>
2016-06-14 23:33:53 -0700

    Initial Commit

---
+++ Tippy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy2
+//
+//  Created by Sumedha Mehta on 6/14/16.
+//  Copyright © 2016 Sumedha Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
